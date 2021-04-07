aa7af9281d76dbb325468fec80cdbd7665502085
Denys Aptikieiev <rtdan333@gmail.com>
2021-04-07 17:11:18 +0300

    finish workspace gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 "use strict";
 let project_folder = "dist";
 let source_folder = "assets";
+let fs = require('fs');
 
 //========================== Объект с путями к файлам start ==============================
 let path = {
@@ -42,7 +43,13 @@ let { src, dest } = require('gulp'),
     // uglify = require('gulp-uglify'),
     uglify_es = require('gulp-uglify-es').default(),
     babel = require('gulp-babel'),
-    image_min = require('gulp-imagemin');
+    image_min = require('gulp-imagemin'),
+    webp = require('gulp-webp'),
+    webp_html = require('gulp-webp-html'),
+    webp_css = require('gulp-webp-css'),
+    ttf2woff = require('gulp-ttf2woff'),
+    ttf2woff2 = require('gulp-ttf2woff2'),
+    fonter = require('gulp-fonter');
 //========================== Подключение Файлов Gulp End ===================================
 
 //========================== Перезагрузка Браузера после обнавления Файлов Star ============
@@ -61,6 +68,7 @@ function browserSync(params) {
 function html() {
     return src(path.src.html)
         .pipe(file_include())
+        .pipe(webp_html())
         .pipe(dest(path.build.html))
         .pipe(browser_sync.stream())
 };
@@ -81,6 +89,7 @@ function css() {
         .pipe(
             groupe_media()
         )
+        .pipe(webp_css())
         .pipe(dest(path.build.css))
         .pipe(rename({
             extname: ".min.css"
@@ -112,16 +121,23 @@ function js() {
 //========================== обработка img Файлов Star =====================================
 function images() {
     return src(path.src.img)
-        .pipe(image_min({
+    .pipe(webp({
+        quality: 70,
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+        image_min({
             progressive: true,
             svgoPlugins: [{
                 removeViewBox: false,
             }],
             interlaced: true,
             optimizationLevel: 5 //0 to 7 
-        }))
-        .pipe(dest(path.build.img))
-        .pipe(browser_sync.stream())
+        })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browser_sync.stream())
 };
 //========================== обработка img Файлов End =======================================
 
@@ -134,16 +150,65 @@ function watchFiles(params) {
 }
 //========================== Отслеживание Файлов End =====================================
 
+//========================== обработка fonts otf Файлов Star ====================================
+gulp.task('otf2ttf', function () {
+    return src([source_folder + '/fonts/*.otf'])
+    .pipe(fonter({
+        formats: ['ttf']
+    }))
+    .pipe(dest([source_folder + '/fonts/']))
+})
+//========================== обработка fonts otf Файлов End =====================================
+
+//========================== обработка fonts Файлов Star ====================================
+function fonts(params) {
+    src(path.src.fonts)
+    .pipe(ttf2woff())
+    .pipe(dest(path.build.fonts))
+    return src(path.src.fonts)
+    .pipe(ttf2woff())
+    .pipe(dest(path.build.fonts))
+}
+//========================== обработка fonts Файлов End ==================================
+
+function fontsStyle() {
+    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+function cb() {
+
+}
+
+
 //========================== Удаление лишних Файлов Star==================================
 function clean() {
     return del(path.clean);
 };
 //========================== Удаление лишних Файлов Star==================================
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images)); //Порядок исполнения
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle); //Порядок исполнения
 let watch = gulp.parallel(build, watchFiles, browserSync); //Порядок исполнения
 
 //========================== Експорт с перерменных и функций Start =======================
+// exports.otf2ttf = otf2ttf;
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
